5b48584a48912f8a156ef69794a180c53bcc3e39
BloodShotMK <rdelaros2404@gmail.com>
2025-03-10 23:03:19 -0600

    Cambios en vistas de navegacion

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav");
 
-// Evento para mostrar/ocultar el menú
+// Evento para mostrar/ocultar el menú en móviles
 menuToggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
